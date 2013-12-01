906cb9d5a76e7c41412d5a4e715e465a176f3fa9
gaston dubra <gaston.dubra@gmail.com>
2013-11-30 22:15:32 -0300

    footer y header hardcoded falta aun

---
+++ wp-content/themes/sye/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="pie">
+			<div class="pie-interno">
+		        <div class="pie-logos"><a href="index.php"  alt="Seguridad y Evaluaci&oacute;n - Sistema Regional de Evaluaci&oacute;n de Impacto en Pol&iacute;ticas de Seguridad Ciudadana" title="Seguridad y Evaluaci&oacute;n - Sistema Regional de Evaluaci&oacute;n de Impacto en Pol&iacute;ticas de Seguridad Ciudadana"></a></div>
+		        <div class="pie-kells"><a href="http://www.kells.com.ar" target="_blank" alt="Kells" title="Kells"></a></div>
+		    </div>
+		</div>
+		</footer>
+	</div><!-- #page -->
+
+	
+</body>
+</html>
--- wp-content/themes/sye/header.php
+++ wp-content/themes/sye/header.php
@@ -33,19 +33,73 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</a>
-
-			<div id="navbar" class="navbar">
-				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-					<?php //wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-					<?php //get_search_form(); ?>
-				</nav><!-- #site-navigation -->
-			</div><!-- #navbar -->
+			<div>
+			<?php
+$arrMesesCompl 	= array('','Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
+?>
+<div class="encabezado-barra">
+	<div class="encabezado-barra-interno">
+		<div class="encabezado-fecha"><?=ucfirst(strtolower($arrDiasCompl[date("w")]))?> <?=date("j")?> de <?=ucfirst(strtolower($arrMesesCompl[ceil(date("m"))]))?> de <?=date("Y")?></div>
+        <div class="encabezado-idioma"><a href="#">English</a></div>
+      <div class="encabezado-redes">
+      	<ul>
+        	<li><a href="#" title="Facebook">&nbsp;</a></li>
+            <li class="twitter"><a href="#" title="Twitter">&nbsp;</a></li>
+            <li class="linkedin"><a href="#" title="LinkedIn">&nbsp;</a></li>
+            <li class="youtube"><a href="#" title="YouTube">&nbsp;</a></li>
+        </ul>
+      </div>
+      <div class="encabezado-buscador">
+      	<form>
+        	<input name="buscador" type="text" />
+        </form>
+      </div>
+      <div class="encabezado-contacto"><a href="contacto.php">Contacto</a></div>
+  </div>
+</div>
+<div class="encabezado">
+  <div class="encabezado-interno">
+    <div class="encabezado-logo"><a href="<?php echo get_home_url()?>"></a></div>
+    <div class="encabezado-logos"><a href="http://www.unvm.edu.ar/" class="unvm_logo" target="_blank"></a></div>
+    <div class="encabezado-logos"><a href="http://www.iadb.org/es" class="iadb_logo" target="_blank" alt="BID - Banco Interamericano de Desarrollo" title="BID - Banco Interamericano de Desarrollo" /></a></div>
+  </div>
+</div>
+<div class="menu">
+  <div class="menu-interno">
+    <ul>
+      <li class="menu-1"><a href="#">Acerca del programa </a>
+        <ul>
+          <li><a href="#">Objetivos y Alcance</a></li>
+          <li><a href="#">Estructura organizacional</a></li>
+          <li><a href="#">Centros de Investigaci&oacute;n Participantes</a></li>
+          <li><a href="#">Otros Gobiernos e Instituciones Adherentes</a></li>
+          <li><a href="#">Anuncios y Agenda</a></li>
+        </ul>
+      </li>
+      <li class="menu-2"><a href="#">Evaluaciones de impacto del programa</a>
+        <ul>
+          <li><a href="#">Programas a evaluar</a></li>
+          <li><a href="#">Dise&ntilde;o</a></li>
+          <li><a href="#">Resultados</a></li>
+        </ul>
+      </li>
+      <li class="menu-3"><a href="#">Blog y novedades</a></li>
+      <li class="menu-4"><a href="#">Publicaciones y recursos</a>
+        <ul>
+          <li><a href="#">Curso de Evaluaci&oacute;n de Programas en Seguridad</a></li>
+          <li><a href="#">Aplicaci&oacute;n para B&uacute;squeda de Pol&iacute;ticas y Evaluaciones</a></li>
+          <li><a href="#">Documentos y Publicaciones de la Red</a></li>
+          <li><a href="#">Material de Capacitaci&oacute;n y Presentaciones</a></li>
+          <li><a href="#">Otras publicaciones</a></li>
+          <li><a href="#">Galer&iacute;a de videos</a></li>
+          <li><a href="#">Galer&iacute;a de fotos</a></li>
+          <li><a href="#">Instituciones y Enlaces Relevantes</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+			</div>
 		</header><!-- #masthead -->
 
 		<div id="main" class="site-main">
