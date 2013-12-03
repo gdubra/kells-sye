be9dc99c73a8026ca3423e7ecb368c2c06ddab3b
gaston dubra <gaston.dubra@gmail.com>
2013-12-03 19:12:28 -0300

    acerca del programa template

--- wp-content/themes/sye/banner-curso-virtual.php
+++ wp-content/themes/sye/banner-curso-virtual.php
@@ -1,5 +1,5 @@
 <div class="columna arriba-30">
 	<a href="http://buenaspracticasenprevencion.org/curso_virtual/entrar.php" target="_blank">
-		<img src="images/banner-curso-virtual-de-capacitacion-en-evaluacion.jpg" width="370" height="59" alt="Curso Virtual de Capacitaci&oacute;n en evaluaci&oacute;n de programas para la prevenci&oacute;n del delito y la violencia en Am&eacute;rica Latina" title="Curso Virtual de Capacitaci&oacute;n en evaluaci&oacute;n de programas para la prevenci&oacute;n del delito y la violencia en Am&eacute;rica Latina" />
+		<img src="/images/banner-curso-virtual-de-capacitacion-en-evaluacion.jpg" width="370" height="59" alt="Curso Virtual de Capacitaci&oacute;n en evaluaci&oacute;n de programas para la prevenci&oacute;n del delito y la violencia en Am&eacute;rica Latina" title="Curso Virtual de Capacitaci&oacute;n en evaluaci&oacute;n de programas para la prevenci&oacute;n del delito y la violencia en Am&eacute;rica Latina" />
 	</a>
 </div>
