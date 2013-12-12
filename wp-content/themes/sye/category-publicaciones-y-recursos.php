270d69d76b4ae9b8c7faf88ce8804eb6a248530f
ezeid <ezeid@despegar.com>
2013-12-12 11:52:09 -0300

    publicaciones y recursos

---
+++ wp-content/themes/sye/category-publicaciones-y-recursos.php
@@ -0,0 +1,44 @@
+<?php
+get_header(); 
+?>
+
+<div class="contenido">
+	<div class="contenido-interno">
+    	<div class="ruta">
+			<ul>
+            	<li><a href="">Portada</a></li>
+            </ul>
+       	</div>
+        <div class="titulo">Blog y Novedades</div>
+        <div class="columna ancho-960">
+        	<div class="interna columna ancho-550 arriba-30">
+        		<?php
+        			$counter = 1; 
+        		     while ( have_posts() ) : the_post(); ?>
+ 					<?php if ($counter % 2  != 0 ) {?> 
+ 						<div class="blog-separador">
+	                		<div class="blog-nota">
+		                    		<?php include('includes/nota-publicaciones-y-recursos.php'); ?>  
+		                	</div>
+                 <?php  } else {?> 
+							<div class="blog-nota izquierda-30">
+		                		<?php include('includes/nota-publicaciones-y-recursos.php'); ?>
+                			</div>
+                		</div>
+                    <?php }; $counter++;?>
+                   
+                <?php endwhile; ?>
+ 
+ 				<?php if ($counter % 2  == 0 ) {?> 
+ 					</div>
+ 				<?php }; ?>
+                <?php include('includes/paginador.php'); ?>                            
+            </div>
+            <div class="columna ancho-370 izquierda-40 barra">
+				<?php echo get_template_part('banner-curso-virtual'); ?>
+				<?php echo get_template_part('publicaciones'); ?>
+				<?php echo get_template_part('tags'); ?>
+            </div>
+        </div>
+	</div>
+</div>
